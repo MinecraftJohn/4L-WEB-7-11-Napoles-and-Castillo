e0cf35f97498df813ff3c887893631b6a78a5c02
John Napoles <84890923+MinecraftJohn@users.noreply.github.com>
2022-09-16 15:37:34 +0800

    delete

--- pagephpvariables/variables.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $authorAlias = "MinecraftJohn";
-    $headermenu1 = "Praesentium";
-    $headermenu2 = "Voluptatum";
-    $headermenu3 = "Dignissimos";
-    $headermenu4 = "Blanditiis";
-?>
--- pagesection/searchsection.php
+++
@@ -1,7 +0,0 @@
-<div class="searchSection">
-    <p class="searchLoadingTitle">☹️</p>
-    <p class="searchLoadingTitle searchLoadingTitleMsg">Oops! No result found.</p>
-    <a class="searchLoadingButton" href="index.php">GO BACK TO HOME PAGE</a>
-    <div class="searchLoading"></div>
-    <span class="searchLoadingText">Searching... Please wait.</span>
-</div>
